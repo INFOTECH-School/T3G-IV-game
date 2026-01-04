c5936132c375d830a58b4c08cede95a61fec196c
Kolbess <roszkomateusz08@gmail.com>
2026-01-04 21:27:14 +0100

    Added GameManager script and its global references for Player. Removed unused tutorialInfo. Added Registering to GameManager by Player and PlayerMovement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,18 @@
+using System;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    private void Awake()
+    {
+        GameManager.Instance.RegisterPlayer(this);
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Instance) GameManager.Instance.UnregisterPlayer();
+    }
+
     void Start()
     {
         
