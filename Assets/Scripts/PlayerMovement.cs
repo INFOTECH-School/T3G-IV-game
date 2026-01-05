637347f0078e9a8b330907453cc53ec6878c1320
Kolbess <122730141+Kolbess@users.noreply.github.com>
2026-01-05 13:24:32 +0100

    Implemented Global References (#8)
    Added GameManager script and its global references for Player. Removed unused tutorialInfo. Added Registering to GameManager by Player and PlayerMovement. These references are accessed with GameManager.Instance.(reference - Player/PlayerMovement)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,18 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private void Awake()
+    {
+        GameManager.Instance.RegisterPlayerMovement(this);
+    }
+    
+    private void OnDestroy()
+    {
+        if (GameManager.Instance) GameManager.Instance.UnregisterPlayerMovement();
+    }
+
     void Update()
     {
         
