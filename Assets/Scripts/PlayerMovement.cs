402abf58673a7a3163dec1482e42676aed7847a5
cionek <marcin.turecki@infotech.edu.pl>
2026-01-10 02:17:40 +0100

    add: added walking and spriting mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,37 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float _acceleration = 10f; 
+    public float runningMultiplier = 1.67f;
+    public float _drag = 1.5f;
+    
+    private Rigidbody _rigidBody;
+    private Vector3 _inputDirection; // direction where player moves
+    private Vector3 _currentVelocity; 
+    
+    private readonly Quaternion _rotation = Quaternion.Euler(0, 45, 0);
+    
     private void Start()
     {
-        GameManager.Instance.RegisterPlayerMovement(this);
+        _rigidBody = GetComponent<Rigidbody>();
     }
     
-    private void OnDestroy()
-    {
-        if (GameManager.Instance) GameManager.Instance.UnregisterPlayerMovement();
-    }
-
     void Update()
     {
+        var rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         
+        _inputDirection = _rotation * rawInput;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 friction = -_rigidBody.linearVelocity * _drag ;
+        _rigidBody.AddForce(friction);
+            
+        float speedMode = Input.GetKey(KeyCode.LeftShift) ? runningMultiplier : 1f;
+        _rigidBody.AddForce(_inputDirection * (_acceleration * speedMode));
     }
 }
