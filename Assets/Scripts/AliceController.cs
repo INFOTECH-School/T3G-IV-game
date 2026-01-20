a5f06892a741b6ae70d562c6336e07f47f0c0018
Kolbess <roszkomateusz08@gmail.com>
2026-01-20 23:21:00 +0100

    Added navmesh surface and configured agent for alice prefab.

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -7,6 +7,7 @@ public class AliceController : MonoBehaviour
     public List<Transform> waypoints;
     public int currentTargetIndex;
     private NavMeshAgent _agent;
+    public bool isLooped = false;
     
     void Start()
     {
@@ -15,17 +16,21 @@ public class AliceController : MonoBehaviour
 
     void MoveToNextPoint()
     {
-        currentTargetIndex++;
-        if (currentTargetIndex >= waypoints.Count)
+        if (currentTargetIndex >= waypoints.Count && isLooped)
         {
             currentTargetIndex = 0;
+        } else if (currentTargetIndex >= waypoints.Count)
+        {
+            return;
         }
         _agent.SetDestination(waypoints[currentTargetIndex].position);
+        currentTargetIndex++;
     }
 
     void Update()
     {
-        if (_agent.remainingDistance <= _agent.stoppingDistance)
+        Debug.Log(_agent.destination);
+        if (_agent.remainingDistance <= _agent.stoppingDistance || _agent.destination == transform.position)
         {
             MoveToNextPoint();
         }
