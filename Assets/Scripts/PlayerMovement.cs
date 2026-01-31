11e4ebd826d4a22265e6c84cb86f2b4a38ff8c6f
ci0nek <marcin.turecki@infotech.edu.pl>
2026-01-31 16:45:53 +0100

    Player/jumping (#16)
    * add: added walking and spriting mechanics
    
    * fix: standarized variable
    
    * add: added jumping mechanic
    
    * Register and unregister player movement with GameManager
    
    * Update Slippery material friction values to zero so player doesnt stick to walls
    
    Set dynamic and static friction to zero for the Slippery material.
    
    * fix: fixed player sticking to walls
    
    * add: added animator script for PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,41 +4,99 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float _acceleration = 10f; 
+    public float _acceleration = 10f;
     public float _runningMultiplier = 1.67f;
     public float _drag = 1.5f;
-    
+    public float _jumpForce = 5f;
+
     private Rigidbody _rigidBody;
-    private Vector3 _inputDirection; // direction where player moves
-    private Vector3 _currentVelocity; 
-    
+    private Vector3 _inputDirection;
+    private Vector3 _currentVelocity;
+    private bool _isGrounded;
+    private bool _isJumpPressed;
+    private int _speedHash;
+    private int _groundedHash;
+    private int _runningHash;
+
+    private Animator _animator;
     private readonly Quaternion _rotation = Quaternion.Euler(0, 45, 0);
-    
+
     private void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        
+        _animator = GetComponent<Animator>();
+        _speedHash = Animator.StringToHash("Speed");
+        _groundedHash = Animator.StringToHash("isGrounded");
+        _runningHash = Animator.StringToHash("isRunning");
+        
         GameManager.Instance.RegisterPlayerMovement(this);
     }
-    
+
     void Update()
     {
         var rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        
         _inputDirection = _rotation * rawInput;
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _isJumpPressed = true;
+        }
+
+        if (_animator)
+        {
+            Vector3 horizontalVelocity = new Vector3(_rigidBody.linearVelocity.x, 0, _rigidBody.linearVelocity.z);
+            float currentSpeed = horizontalVelocity.magnitude;
+
+            if (currentSpeed < 0.1f) currentSpeed = 0.0f;
+            _animator.SetFloat(_speedHash, currentSpeed);
+            _animator.SetBool(_groundedHash, _isGrounded);
+            _animator.SetBool(_runningHash, Input.GetKey(KeyCode.LeftShift));
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector3 friction = -_rigidBody.linearVelocity * _drag ;
+    /* friction */
+        Vector3 currentVelocity = _rigidBody.linearVelocity;
+        Vector3 horizontalVelocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
+        var friction = -horizontalVelocity * _drag;
+        
         _rigidBody.AddForce(friction);
-            
+        
+    /* movement */
+        
         float speedMode = Input.GetKey(KeyCode.LeftShift) ? _runningMultiplier : 1f;
         _rigidBody.AddForce(_inputDirection * (_acceleration * speedMode));
+        
+    /* jumping */
+        if (_isJumpPressed)
+        {
+            _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isJumpPressed = false;
+            _isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            if (collision.GetContact(i).normal.y > 0.7f)
+            {
+                _isGrounded = true;
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionExit()
+    {
+        _isGrounded = false;
     }
 
     private void OnDestroy()
     {
         if (GameManager.Instance) GameManager.Instance.UnregisterPlayerMovement();
     }
-
 }
