435ba171a5b2c863d80a368a609343f2bd21262e
mateusz-roszko <roszkomatuesz08@gmail.com>
2026-03-13 16:59:21 +0100

    Created new management prefabs, Improved tutorial level.

--- Assets/Scripts/LevelObjective.cs
+++ Assets/Scripts/LevelObjective.cs
@@ -1,15 +1,29 @@
+using System;
 using UnityEngine;
 
 public class LevelObjective : MonoBehaviour
 {
     public bool truckObjective;
+    public GameObject finishResult;
+
+    private void Start()
+    {
+        if (finishResult) finishResult.SetActive(false);
+    }
+
     public void CompleteObjective()
     {
         if (truckObjective)
         {
             GameManager.Instance.LevelOperator.ProgressTruck();
         }
-        GameManager.Instance.LevelOperator.ProgressLevel();
+
+        if (finishResult) finishResult.SetActive(true);
+        if (GameManager.Instance.LevelOperator)
+        {
+            GameManager.Instance.LevelOperator.ProgressLevel();
+        }
+
         // Optionally, disable this component to prevent repeated calls
         this.enabled = false;
     }
